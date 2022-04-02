317467b1de39438531a0edc637638093da7fcb5e
Vladick <cool.valad12@gmail.com>
2022-04-02 22:09:19 +0300

    Add Service

--- src/main/java/com/epam/webapp/controller/MainController.java
+++ src/main/java/com/epam/webapp/controller/MainController.java
@@ -1,8 +1,10 @@
 package com.epam.webapp.controller;
 import com.epam.webapp.domain.Message;
+import com.epam.webapp.domain.User;
 import com.epam.webapp.repos.MessageRepo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,7 +20,7 @@ public class MainController {
 
     @GetMapping("/")
     public String greeting(Map<String, Object> model) {
-        return "greeting";
+        return "greeting.ftl";
     }
     @GetMapping("/main")
     public String main(String main, Map<String, Object> model){
@@ -28,8 +30,12 @@ public class MainController {
     }
 
     @PostMapping("/main")
-    public String add(@RequestParam String text, @RequestParam String tag ,Map<String, Object> model){
-        Message message = new Message(text, tag);
+    public String add(
+            @AuthenticationPrincipal User user,
+            @RequestParam String text,
+            @RequestParam String tag ,Map<String, Object> model
+    ){
+        Message message = new Message(text, tag, user);
         messageRepo.save(message);
 
         Iterable<Message> messages = messageRepo.findAll();
