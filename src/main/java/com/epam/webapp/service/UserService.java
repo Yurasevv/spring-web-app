3dbbe92f18e2e01eb2a3ceb06938a16fab149b35
Vladick <cool.valad12@gmail.com>
2022-07-12 23:38:35 +0300

    Add email activation

--- src/main/java/com/epam/webapp/service/UserService.java
+++ src/main/java/com/epam/webapp/service/UserService.java
@@ -1,19 +1,63 @@
 package com.epam.webapp.service;
 
+import com.epam.webapp.domain.Role;
+import com.epam.webapp.domain.User;
 import com.epam.webapp.repos.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
+
+import java.util.Collections;
+import java.util.UUID;
 
 @Service
 public class UserService implements UserDetailsService {
+    @Autowired
+    private MailSender mailSender;
     @Autowired
     private UserRepo userRepo;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return userRepo.findByUsername(username);
+    }
+
+    public boolean addUser(User user){
+        User userFromDb = userRepo.findByUsername(user.getUsername());
+
+        if(userFromDb != null) {
+            return false;
+        }
+        user.setActive(true);
+        user.setRoles(Collections.singleton(Role.USER));
+        user.setActivationCode(UUID.randomUUID().toString());
+
+        userRepo.save(user);
+
+        if (!ObjectUtils.isEmpty(user.getEmail())){
+            String message = String.format(
+                    "Hello, %s \n" +
+                            "Please, visit next link: http://localhost:8080/activate/%s",
+                    user.getUsername(),
+                    user.getActivationCode()
+            );
+            mailSender.send(user.getEmail(), "Activation code", message);
+        }
+
+        return true;
+    }
+
+    public boolean activateUser(String code) {
+        User user = userRepo.findByActivationCode(code);
+        if (user == null) {
+            return false;
+        }
+        user.setActivationCode(null);
+        userRepo.save(user);
+
+        return true;
     }
 }
